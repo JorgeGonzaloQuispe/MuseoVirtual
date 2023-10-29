e2c9aedaee1ca221855c926fcfb15c144c7b42e5
Irán María Alvarez Flores <iran.alvarez@utec.edu.pe>
2023-10-28 21:57:37 -0500

    nuevos cubos

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 app = Ursina()
 
 player = FirstPersonController(speed=80, position=(-80, 9, -240), scale=9)
-
 Sky()
 
 def input(key):
@@ -13,4 +12,9 @@ def input(key):
 
 # Piso
 platform = Entity(model="cube", collider="box", texture="piso", scale=(3000, 0, 3000), position=(0, 10, 0))
+
+Entity(model="cube", collider="mesh", texture="pared", scale=250,position=(250, 0, 0))#pared derecha
+Entity(model="cube", collider="mesh", texture="techo", scale=250,position=(0, 250, 0))#techo
+Entity(model="cube", collider="mesh", texture="pared", scale=250,position=(-250, 0, 0))# pared izquierda
+Entity(model="cube", collider="mesh", texture="pared", scale=250,position=(0, 0, 250))
 app.run()
