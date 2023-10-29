9ebbddfd86d09f9891075d1ad76232084ced0dc6
cinthiarosales1 <cinthia.rosales@utec.edu.pe>
2023-10-28 21:53:30 -0500

    agregando un cubo

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 app = Ursina()
 
 player = FirstPersonController(speed=80, position=(-80, 9, -240), scale=9)
-
 Sky()
 
 def input(key):
@@ -13,4 +12,8 @@ def input(key):
 
 # Piso
 platform = Entity(model="cube", collider="box", texture="piso", scale=(3000, 0, 3000), position=(0, 10, 0))
+
+#Cubo
+Entity(model="cube", collider="mesh", texture="holi", scale=250,position=(250, 250, 0))#pared derecha
+
 app.run()
