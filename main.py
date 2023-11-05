f0b8908786febb057a1f0b28cd55d0f6621bac52
Irán María Alvarez Flores <iran.alvarez@utec.edu.pe>
2023-11-04 19:13:52 -0500

    updated museum base

--- main.py
+++ main.py
@@ -3,19 +3,40 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
-player = FirstPersonController(speed=80, position=(-80, 9, -240), scale=9)
+player = FirstPersonController(speed=150, position=(50, 0, 50), scale=9)  # Nueva posición del jugador
 Sky()
 
 def input(key):
     if key == 'escape':
         quit()
 
-# Piso
-platform = Entity(model="cube", collider="box", texture="piso", scale=(3000, 0, 3000), position=(0, 10, 0))
+# Crear el suelo del museo
+platform = Entity(model="cube", collider="box", texture="piso", scale=(3000, 1, 3000), position=(0, 0, 0))
 
-#Cubo
-Entity(model="cube", collider="mesh", texture="holi", scale=250,position=(250, 250, 0))#pared derecha
-Entity(model="cadasdasdube", collider="mesh", texture="holi", scale=250,position=(500, 250, 0))#pared derecha 
+# Pared derecha
+wall_1 = Entity(model='cube', scale=(1000, 1000, 10), color=color.white, texture = "wall4.jpg", collider='box')
+wall_1.x = 0  # Ubicar la tercera pared
+wall_1.z = -500
+wall_1.y = 0
+
+# Pared izquierda
+wall_2 = Entity(model='cube', scale=(1000, 1000, 10), color=color.white, texture = "wall4.jpg", collider='box')
+wall_2.x = 0  # Ubicar la tercera pared
+wall_2.z = 500
+wall_2.y = 0
+
+# Pared enfrente
+wall_2 = Entity(model='cube', scale=(10, 1000, 1000), color=color.white, texture = "wall4.jpg", collider='box')
+wall_2.x = -500  # Ubicar la tercera pared
+wall_2.z = 0
+wall_2.y = 0
+
+# Techo del museo
+ceiling = Entity(model='cube', scale=(1000, 10, 1000), color=color.white, texture = "wall1", collider='box')
+ceiling.y = 500  # Ubicar el techo
+ceiling.z = 0
+ceiling.x = 0
+
+#Colocar aqui sus cambios 
 
 app.run()
-
