18989ce5f107ad6b27157b07648b2e792639e041
cinthiarosales1 <cinthia.rosales@utec.edu.pe>
2023-11-04 20:03:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ def input(key):
 platform = Entity(model="cube", collider="box", texture="piso", scale=(3000, 0, 3000), position=(0, 10, 0))
 
 #Cubo
-Entity(model="cube", collider="mesh", texture="holi", scale=250,position=(250, 250, 0))#pared derecha
+#Entity(model="cube", collider="mesh", texture="holi", scale=250,position=(250, 250, 0))#pared derecha
+#Entity(model="cube", collider="mesh", texture="holi", scale=250,position=(500, 250, 0))#pared derecha 
+#pared derecha
+Entity(model="cube", collider="mesh", texture="holi", scale=250,position=(500, 250, 0))#pared derecha
 Entity(model="cube", collider="mesh", texture="holi", scale=250,position=(500, 250, 0))#pared derecha 
 
 app.run()
